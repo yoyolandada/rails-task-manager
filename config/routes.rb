06b9be8463dfddddb8cf457c3cf53a85a55a8271
yolanda martinez priego <yulielx@gmail.com>
2024-01-23 15:18:02 +0000

    done from 1 to 6

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "tasks", to:"tasks#index"
+  get "tasks/new", to:"tasks#new"
+  post "tasks", to:"tasks#create"
   get "tasks/:id", to:"tasks#show", as: :task
+
+
 end
